4591e445e991061f0225193938a4c65d63954952
segcore <segcore@github.com>
2024-05-15 10:14:32 +0800

    Bash, gitconfig, tmux

---
+++ bash/custom.sh
@@ -0,0 +1,30 @@
+export EDITOR=nvim
+export CMAKE_GENERATOR=Ninja
+
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@ubuntu\[\033[00m\]:\[\033[01;34m\]\w\[\033[0;33m\]$(__git_ps1 " (🌿 %s)")\[\033[00m\]\$ '
+
+alias vim='nvim'
+alias bat='batcat --theme Coldark-Cold'
+alias delta='delta --syntax-theme=gruvbox-light'
+alias human='numfmt --to=si'
+
+# Get ancestor build directory
+bdir() {
+    pwd | sed 's#\(/build[^/]*\)/.*#\1#'
+}
+alias cb='cmake --build `bdir` --parallel'
+
+function ask() {
+    echo "   $@"
+    read -p "Run this command? [Y/n]" WORD
+    if [ "$WORD" = "Y" -o "$WORD" = "y" -o -z "$WORD" ]; then
+        "$@"
+    fi
+}
+
+function prune() {
+    local branches="$(git branch -v | awk '/\w+\s+\w+.* \[gone\]/ { print $1 }')"
+    if [ -n "$branches" ]; then
+        ask git branch -d $branches
+    fi
+}
